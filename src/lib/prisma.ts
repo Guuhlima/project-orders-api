90c1367ce56d2d87713197f6e586fd264aee6dac
Gustavo <guuhalves0922@gmail.com>
2026-03-04 20:47:22 -0300

    prisma(fix): fix prisma and go deploy

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,5 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
 
 @Injectable()
 export class PrismaService 
@@ -9,17 +7,11 @@ export class PrismaService
     implements OnModuleInit, OnModuleDestroy
 {
     constructor() {
-        const connectionString = process.env.DATABASE_URL;
-        if (!connectionString) throw new Error('DATABASE_URL is not defined');
+        if (!process.env.DATABASE_URL) {
+            throw new Error('DATABASE_URL is not defined');
+        }
 
-        const pool = new Pool({ connectionString });
-
-        pool.on('connect', (client) => {
-            void client.query('SET search_path TO orders, public');
-        });
-
-        const adapter = new PrismaPg(pool);
-        super({ adapter });
+        super({});
     }
 
     async onModuleInit() {
